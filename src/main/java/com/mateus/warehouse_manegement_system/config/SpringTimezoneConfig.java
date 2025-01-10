9b8943ebac8c534d54fb822d39f0189b53a3545e
Mateus Oliveira <mateuskywalker90@gmail.com>
2025-01-10 19:48:00 -0400

    created timezone configuration

---
+++ src/main/java/com/mateus/warehouse_manegement_system/config/SpringTimezoneConfig.java
@@ -0,0 +1,15 @@
+package com.mateus.warehouse_manegement_system.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimezoneConfig {
+
+    @PostConstruct
+    public void timeZoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+04:00"));
+    }
+}
