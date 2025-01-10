aa814cb4635cbafcd12e0e073ba61c0ce070a19a
Mateus Oliveira <mateuskywalker90@gmail.com>
2025-01-10 18:34:14 -0400

    uploading the project to the repository

---
+++ src/main/java/com/mateus/warehouse_manegement_system/WarehouseManegementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.warehouse_manegement_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManegementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManegementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateus/warehouse_manegement_system/WarehouseManegementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.warehouse_manegement_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManegementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
