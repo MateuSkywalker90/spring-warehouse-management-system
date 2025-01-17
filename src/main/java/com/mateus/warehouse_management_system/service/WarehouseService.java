049714a37d83030e0329eea912e8c325a3fe13c0
Mateus Oliveira <mateuskywalker90@gmail.com>
2025-01-17 19:35:52 -0400

    feat: created warehouse save method

--- src/main/java/com/mateus/warehouse_management_system/service/WarehouseService.java
+++ src/main/java/com/mateus/warehouse_management_system/service/WarehouseService.java
@@ -1,11 +1,19 @@
 package com.mateus.warehouse_management_system.service;
 
+import com.mateus.warehouse_management_system.entity.Warehouse;
+import com.mateus.warehouse_management_system.repository.WarehouseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class WarehouseService {
 
-    //private final WarehouseRepository warehouseRepository;
+    private final WarehouseRepository warehouseRepository;
+
+    @Transactional
+    public Warehouse save(Warehouse warehouse) {
+        return warehouseRepository.save(warehouse);
+    }
 }
--- src/main/java/com/mateus/warehouse_management_system/web/controller/WarehouseController.java
+++ src/main/java/com/mateus/warehouse_management_system/web/controller/WarehouseController.java
@@ -1,6 +1,12 @@
 package com.mateus.warehouse_management_system.web.controller;
 
+import com.mateus.warehouse_management_system.entity.Warehouse;
+import com.mateus.warehouse_management_system.service.WarehouseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/warehouses")
 public class WarehouseController {
 
-    //private final WarehouseService warehouseService;
+    private final WarehouseService warehouseService;
+
+    @PostMapping
+    public ResponseEntity<Warehouse> create(@RequestBody Warehouse warehouse) {
+        Warehouse response = warehouseService.save(warehouse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
