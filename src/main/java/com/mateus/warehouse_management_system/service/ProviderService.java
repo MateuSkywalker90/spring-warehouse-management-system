830f615e512d6c9e6717fcefb6be12bb2a1caeae
Mateus Oliveira <mateuskywalker90@gmail.com>
2025-01-17 19:30:52 -0400

    feat: created provider save method

--- src/main/java/com/mateus/warehouse_management_system/service/ProviderService.java
+++ src/main/java/com/mateus/warehouse_management_system/service/ProviderService.java
@@ -1,11 +1,19 @@
 package com.mateus.warehouse_management_system.service;
 
+import com.mateus.warehouse_management_system.entity.Provider;
+import com.mateus.warehouse_management_system.repository.ProviderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class ProviderService {
 
-    //private final ProviderRepository providerRepository;
+    private final ProviderRepository providerRepository;
+
+    @Transactional
+    public Provider save(Provider provider) {
+        return providerRepository.save(provider);
+    }
 }
--- src/main/java/com/mateus/warehouse_management_system/web/controller/ProviderController.java
+++ src/main/java/com/mateus/warehouse_management_system/web/controller/ProviderController.java
@@ -1,6 +1,12 @@
 package com.mateus.warehouse_management_system.web.controller;
 
+import com.mateus.warehouse_management_system.entity.Provider;
+import com.mateus.warehouse_management_system.service.ProviderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/providers")
 public class ProviderController {
 
-    //private final ProviderService providerService;
+    private final ProviderService providerService;
+
+    @PostMapping
+    public ResponseEntity<Provider> create(@RequestBody Provider provider) {
+        Provider response = providerService.save(provider);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
